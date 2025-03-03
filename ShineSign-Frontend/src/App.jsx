3dcc24da8f259bce25dce61a2efc03d4755690cd
Freskies <43386854+Freskies@users.noreply.github.com>
2025-03-03 15:19:13 +0100

    create a base html editor

--- ShineSign-Frontend/src/App.jsx
+++ ShineSign-Frontend/src/App.jsx
@@ -1,5 +1,49 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { useRef } from "react";
+import { Editor } from "@monaco-editor/react";
+import { emmetHTML, emmetCSS } from "emmet-monaco-es";
+
 export default function App () {
-	return <>
-		App
-	</>;
+	const page = useRef(null);
+	const editor = useRef(null);
+
+	function handleEditorMount () {
+		emmetHTML(window.monaco);
+		emmetCSS(window.monaco);
+	}
+
+	async function handlePDF () {
+		const canvas = await html2canvas(page.current);
+		const imgCanvas = canvas.toDataURL("image/png");
+		const pdf = new jsPDF();
+		pdf.addImage(imgCanvas, "JPEG", 0, 0);
+		pdf.save("download.pdf");
+	}
+
+	// display the code in the iframe
+	function display (content = "") {
+		if (!page.current) return;
+		page.current.srcdoc = `<html><body>${content}</body></html>`;
+	}
+
+	return <div className="editor-page">
+		<Editor
+			ref={editor}
+			className="editor"
+			defaultLanguage="html"
+			defaultValue="<!-- Write your code here -->"
+			onChange={value => display(value)}
+			options={{
+				quickSuggestions: true,
+				wordBasedSuggestions: "currentDocument",
+				suggestOnTriggerCharacters: true,
+				tabCompletion: "on",
+				minimap: { enabled: false },
+			}}
+			onMount={handleEditorMount}
+		/>
+		<iframe className="pdf-page" ref={page}></iframe>
+	</div>;
+
 };
